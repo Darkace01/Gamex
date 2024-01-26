29455b15deaf30db0a6e2a103d271e2ae105753a
Kazeem Quadri <quadrikazeem01@gmail.com>
2024-01-26 10:19:18 +0100

    Edit post

--- Gamex.DTO/PostDTO.cs
+++ Gamex.DTO/PostDTO.cs
@@ -8,6 +8,7 @@ public class PostDTO
     public string Title { get; set; } = string.Empty;
     public string Content { get; set; } = string.Empty;
     public bool? IsArchived { get; set; } = false;
+    public Guid PictureId { get; set; }
     public string PictureUrl { get; set; } = string.Empty;
     public string PicturePublicId { get; set; } = string.Empty;
     public UserProfileDTO User { get; set; } = new();
@@ -17,12 +18,13 @@ public class PostDTO
     {
     }
 
-    public PostDTO(Guid id, string title, string content, bool? isArchived, string pictureUrl, string picturePublicId, UserProfileDTO user, IEnumerable<CommentDTO> comment)
+    public PostDTO(Guid id, string title, string content, bool? isArchived,Guid pictureId, string pictureUrl, string picturePublicId, UserProfileDTO user, IEnumerable<CommentDTO> comment)
     {
         Id = id;
         Title = title;
         Content = content;
         IsArchived = isArchived;
+        PictureId = pictureId;
         PictureUrl = pictureUrl;
         PicturePublicId = picturePublicId;
         User = user;
