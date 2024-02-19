b5eedafd1f5254fdd406e0f6acac7f27a34f5129
Kazeem Quadri <quadrikazeem01@gmail.com>
2024-02-19 08:58:39 +0100

    The most significant changes involve the addition of new properties to the `Tournament` and `UserTournaments` models, the creation of indexes for these properties to improve query performance, and the creation of a new migration to implement these changes in the database.

---
+++ Gamex.Data/Migrations/20240219075631_TournamentCoverImage.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gamex.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TournamentCoverImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatorId",
+                table: "UserTournaments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CoverPictureId",
+                table: "Tournaments",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tournaments_CoverPictureId",
+                table: "Tournaments",
+                column: "CoverPictureId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tournaments_Pictures_CoverPictureId",
+                table: "Tournaments",
+                column: "CoverPictureId",
+                principalTable: "Pictures",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tournaments_Pictures_CoverPictureId",
+                table: "Tournaments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tournaments_CoverPictureId",
+                table: "Tournaments");
+
+            migrationBuilder.DropColumn(
+                name: "CreatorId",
+                table: "UserTournaments");
+
+            migrationBuilder.DropColumn(
+                name: "CoverPictureId",
+                table: "Tournaments");
+        }
+    }
+}
