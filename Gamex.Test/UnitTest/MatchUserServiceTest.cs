7d86c1fd4e20cca535bbd87fdb2676a945d77d9d
Kazeem Quadri <quadrikazeem01@gmail.com>
2024-05-09 12:55:26 +0100

    The most significant changes involve the addition of new classes and interfaces to manage match users in the `Gamex.DTO`, `Gamex.Service.Contract`, and `Gamex.Service.Implementation` namespaces. These changes include the creation of data transfer objects for match users, the definition of a service contract for managing match users, and the implementation of this service contract. Additionally, unit tests for the new service were added.
    1. A new `MatchUserDTO` class was added to the `Gamex.DTO` namespace. This class is a data transfer object for match users and includes various properties and constructors.
    2. Two new classes, `MatchUserCreateDTO` and `MatchUserUpdateDTO`, were added to the `Gamex.DTO` namespace. These classes are data transfer objects for creating and updating match users.
    3. A new `IMatchUserService` interface was added to the `Gamex.Service.Contract` namespace. This interface defines the contract for a service that manages match users.
    4. A new `MatchUserService` class was added to the `Gamex.Service.Implementation` namespace. This class implements the `IMatchUserService` interface and provides the logic for managing match users.
    5. A new `MatchUserServiceTest` class was added to the `Gamex.Test.UnitTest` namespace. This class includes unit tests for the `MatchUserService` class.

---
+++ Gamex.Test/UnitTest/MatchUserServiceTest.cs
@@ -0,0 +1,103 @@
+﻿namespace Gamex.Test.UnitTest;
+
+public class MatchUserServiceTest : TestBase
+{
+    [Fact]
+    public async Task CreateMatchUser_ShouldCreateMatchUser()
+    {
+        // Arrange
+        var dbContext = GetSampleData(nameof(CreateMatchUser_ShouldCreateMatchUser));
+        var matchUserService = MockMatchUserService(dbContext);
+        var match = dbContext.RoundMatches.FirstOrDefault();
+        var userInTournament = dbContext.UserTournaments.FirstOrDefault();
+        MatchUserCreateDTO newMatchUser = new()
+        {
+            MatchId = match.Id,
+            UserId = userInTournament.UserId
+        };
+
+        // Act
+        var (created, message) = await matchUserService.CreateMatchUser(newMatchUser);
+
+        // Assert
+        Assert.True(created);
+        var matchUserInDb = dbContext.MatchUsers.FirstOrDefault();
+        Assert.NotNull(matchUserInDb);
+    }
+
+    [Fact]
+    public async Task UpdateMatchUser_ShouldUpdateMatchUser()
+    {
+        // Arrange
+        var dbContext = GetSampleData(nameof(UpdateMatchUser_ShouldUpdateMatchUser));
+        var matchUserService = MockMatchUserService(dbContext);
+        var matchUserToUpdate = dbContext.MatchUsers.FirstOrDefault();
+        MatchUserUpdateDTO updatedMatchUser = new()
+        {
+            Id = matchUserToUpdate.Id,
+            Points = 100
+        };
+
+        // Act
+        var (updated, message) = await matchUserService.UpdateMatchUser(updatedMatchUser);
+
+        // Assert
+        var updatedMatchUserInDb = dbContext.MatchUsers.FirstOrDefault(m => m.Id == matchUserToUpdate.Id);
+        Assert.True(updated);
+        Assert.Equal(updatedMatchUser.Points, updatedMatchUserInDb.Point);
+    }
+
+    [Fact]
+    public async Task DeleteMatchUser_ShouldDeleteMatchUser()
+    {
+        // Arrange
+        var dbContext = GetSampleData(nameof(DeleteMatchUser_ShouldDeleteMatchUser));
+        var matchUserService = MockMatchUserService(dbContext);
+        var matchUserToDelete = dbContext.MatchUsers.FirstOrDefault();
+
+        // Act
+        var (deleted, message) = await matchUserService.DeleteMatchUser(matchUserToDelete.Id);
+
+        // Assert
+        var deletedMatchUserInDb = dbContext.MatchUsers.FirstOrDefault(m => m.Id == matchUserToDelete.Id);
+        Assert.True(deleted);
+        Assert.Null(deletedMatchUserInDb);
+    }
+
+    [Fact]
+    public void GetMatchUsersByMatchId_ShouldReturnMatchUsers()
+    {
+        // Arrange
+        var dbContext = GetSampleData(nameof(GetMatchUsersByMatchId_ShouldReturnMatchUsers));
+        var matchUserService = MockMatchUserService(dbContext);
+        var matchId = dbContext.RoundMatches.FirstOrDefault(x => x.MatchUser.Any()).Id;
+
+        // Act
+        var matchUsers = matchUserService.GetMatchUsersByMatchId(matchId);
+
+        // Assert
+        Assert.NotEmpty(matchUsers);
+    }
+
+    [Fact]
+    public async Task GetMatchUsersById_ShouldReturnMatchUser()
+    {
+        // Arrange
+        var dbContext = GetSampleData(nameof(GetMatchUsersById_ShouldReturnMatchUser));
+        var matchUserService = MockMatchUserService(dbContext);
+        var matchUserId = dbContext.MatchUsers.FirstOrDefault().Id;
+
+        // Act
+        var matchUser = await matchUserService.GetMatchUsersById(matchUserId);
+
+        // Assert
+        Assert.NotNull(matchUser);
+    }
+
+    #region Helpers
+    private IMatchUserService MockMatchUserService(GamexDbContext context)
+    {
+        return new MatchUserService(context);
+    }
+    #endregion
+}
