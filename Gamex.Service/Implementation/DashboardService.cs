d44444b0986bde6ccf069f2502c7a9c78f42dd0a
Kazeem Quadri <quadrikazeem01@gmail.com>
2024-05-11 22:03:13 +0100

    Dashboard Stats

---
+++ Gamex.Service/Implementation/DashboardService.cs
@@ -0,0 +1,19 @@
+﻿namespace Gamex.Service.Implementation;
+public class DashboardService(GamexDbContext context) : IDashboardService
+{
+    public async Task<DashboardDTO> GetDashboardStats(CancellationToken cancellationToken = default)
+    {
+        var registeredUserCount = await context.Users.CountAsync(cancellationToken);
+        var tournamentCount = await context.Tournaments.CountAsync(cancellationToken);
+        var usersInWaitList = await context.UserTournaments.CountAsync(ut => ut.WaitList ?? false, cancellationToken);
+        var postsCount = await context.Posts.CountAsync(cancellationToken);
+
+        return new DashboardDTO
+        {
+            RegisteredUsers = registeredUserCount,
+            Tournaments = tournamentCount,
+            UsersInWaitlist = usersInWaitList,
+            Posts = postsCount
+        };
+    }
+}
